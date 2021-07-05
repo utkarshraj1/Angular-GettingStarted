fd29c410d33241e46140a4a27dd32f457802bca2
Utkarsh Raj <utkarsh.raj@outlook.com>
2021-07-05 13:01:46 +0530

    Dependency injection applied

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -16,59 +17,18 @@ export class ProductListComponent implements OnInit {
   showImage: boolean = false;
   //filterValue: string = '';
   private _filterValue: string = '';
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2021",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2020",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
-  filteredProducts: IProduct[] = this.products;
+  products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
+
+  //Constructor
+  constructor(private productService: ProductService) {
+  }
+  /*Above code is the shorthand technique of :
+  private _productService;
+  constructor(productService: ProductService) {
+    this._productService = productService;
+  }
+  */
 
   //Getters and Setters
   get filterValue(): string {
@@ -92,8 +52,11 @@ export class ProductListComponent implements OnInit {
   starClickEvent(value: string): void {
     this.starValue = value;
   }
-  ngOnInit(): void {
-    console.log("ngOnInIt method, to be used later");
 
+  //Lifecycle Hooks
+  ngOnInit(): void {
+    //console.log("ngOnInIt method, to be used later");
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
