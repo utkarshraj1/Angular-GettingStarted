40f9df5373a4c3017503dcc88267709a59bc57f8
Utkarsh Raj <utkarsh.raj@outlook.com>
2021-07-06 23:34:02 +0530

    Made shared and feature modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
-import { StarComponent } from './shared/star.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    WelcomeComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', 
-        canActivate: [ProductDetailGuard], 
-        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ConvertToSpacesPipe } from "./convert-to-space.pipe";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+    declarations: [
+        StarComponent,
+        ConvertToSpacesPipe
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        StarComponent,
+        CommonModule,
+        FormsModule,
+        ConvertToSpacesPipe
+    ]
+})
+export class SharedModule {
+
+}
