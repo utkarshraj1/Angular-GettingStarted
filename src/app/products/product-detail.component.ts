3a52e13b0af99d131ffa85f3e52b6ea459632845
Utkarsh Raj <utkarsh.raj@outlook.com>
2021-07-06 21:15:51 +0530

    Routing extended with more applications

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IProduct } from "./product";
 
 @Component({
     templateUrl: './product-detail.component.html',
     styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
     titlePage: string = "Product Details";
+    product: IProduct | undefined;
+
+    constructor(private route: ActivatedRoute,
+                private routeBack: Router) {
+    }
+
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.titlePage += `: ${id}`;
+    }
+
+    onBack(): void {
+        this.routeBack.navigate(['/products']);
+    }
 }
